771daaf71ec0d73528e5bd3002eefbc31b30b62f
Angela Lo <angelawenlo@gmail.com>
2019-06-28 20:18:00 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,7 +3,6 @@ class Person
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
 
   def initialize(attributes)
-    if attributes
       attributes.each do {|k, v| self.send(("#{key}="), value)}
       end
   end
