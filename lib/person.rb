1fce53500391458f6687abdc45f683762e3ec803
Angela Lo <angelawenlo@gmail.com>
2019-06-28 20:17:57 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,9 +2,9 @@ class Person
   #your code here
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
 
-  def initialize(attributes = nil)
+  def initialize(attributes)
     if attributes
-      attributes.each do {|key, value| self.send(("#{key}="), value)}
+      attributes.each do {|k, v| self.send(("#{key}="), value)}
       end
   end
 end
